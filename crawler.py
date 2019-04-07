013523e16770bc4fec25eba3bad658169982f924
Rex Zhang <rex_fa@hotmail.com>
2019-04-07 23:32:58 +0800

    学习

--- crawler.py
+++ crawler.py
@@ -1,2 +1,14 @@
 import requests
 import json
+import lxml
+# pip install -i requests https://pypi.douban.com/simple
+# pip install -i https://pypi.douban.com/simple pandas 
+# pip install lxml
+# https://zhuanlan.zhihu.com/p/32037625
+
+url = 'https://movie.douban.com/subject/1292052/'
+data = requests.get(url).text
+s=etree.HTML(data)
+
+film=s.xpath('//*[@id="content"]/h1/span[1]/text()')
+print(film)
