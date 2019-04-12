add0c4089de21f40e3f8a9d1e3c890563e657e70
Rex Zhang <rex_fa@hotmail.com>
2019-04-13 00:03:54 +0800

    设计

--- crawlerClass.py
+++ crawlerClass.py
@@ -6,5 +6,9 @@ class crawlerClass:
     __url=''
     def __init__(self,url):
         self.__url = url
-    def crawlerAndSave(self):
-        
+    def crawlerHtml(self,xpathTitle,xpathContent):
+        htmlData=requests.get(self.__url).text
+        s=etree.HTML(htmlData)
+        docTitle = s.xpath(xpathTitle)
+        docContent = s.xpath(xpathContent)
+        return docTitle,docContent
