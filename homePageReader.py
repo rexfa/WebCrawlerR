918a1c3390b0a0d3c62759e9c594c6c668acb964
Rex Zhang <rex_fa@hotmail.com>
2019-04-16 23:23:43 +0800

    试验1

--- homePageReader.py
+++ homePageReader.py
@@ -12,22 +12,25 @@ class homePageReader:
         if(isLocal):
             html = urllib.request.FileHandler()
             req = urllib.request.Request(self.__url)
-            html.file_open(req).read()
-            links = re.findall('"((http)s?://.*?)"', html)
+            html.file_open(req).read().decode('UTF-8')
+            links = re.findall('"((http|ftp)s?://.*?)"', html)
             print(links)
         else:
             website = urllib.request.urlopen(self.__url)
         #read html code
-            html = website.read()
+            html = website.read().decode('UTF-8')
         #use re.findall to get all the links
         #links = re.findall('"((http|ftp)s?://.*?)"', html)
-            links = re.findall('"((http)s?://.*?)"', html)
+            #print(html)
+            mores = re.findall('"((http|ftp)s?更多)"',html)
+            links = re.findall('"((http|ftp)s?://.*?)"', html)
         #htmlData=requests.get(self.__url).text
         #s=etree.HTML(htmlData)
-            print(links)
+            #print(links)
+            print(mores)
 
 
-a = homePageReader('D:/VSCode/Python/WebCrawlerR/ExHTML/科研信息_中国农业科学院作物科学研究所内网.html')
-a.analysis(True)
+a = homePageReader('file:/D:/VSCode/Python/WebCrawlerR/ExHTML/中国农业科学院作物科学研究所内网.html')
+a.analysis(False)
         
 
